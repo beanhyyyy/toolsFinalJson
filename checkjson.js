5b9f1a29672cbfa44d752f385c8544dc115d2095
nguyen <quangnguyenne1411@gmail.com>
2021-04-22 15:34:29 +0700

    Update

---
+++ checkjson.js
@@ -0,0 +1,60 @@
+const json1 = [
+  { KEY: "VN" },
+  { "accountSetting.generalTab.capEnterUserID": "" },
+  {
+    "accountSetting.generalTab.contentChangeName":
+      "Vì vấn đề bảo mật, vui lòng nhập lại mật khẩu để tiếp tục.",
+  },
+  {
+    "accountSetting.generalTab.contentUserID":
+      "Tên người dùng phải trùng với tên hiển thị trên dòng thời gian của bạn",
+  },
+];
+
+const json3 = [
+  { KEY: "VN" },
+  { "accountSetting.generalTab.capEnterUserID": "" },
+  {
+    "accountSetting.generalTab.contentChangeName":
+      "Vì vấn đề bảo mật, vui lòng nhập lại mật khẩu để tiếp tục.",
+  },
+  {
+    "accountSetting.generalTab.contentUserID":
+      "Tên người dùng phải trùng với tên hiển thị trên dòng thời gian của bạn",
+  },
+  {
+    "accountSetting.generalTab.errChangeName":
+      "Thay đổi tên thất bại. Mật khẩu bạn nhập không chính xác",
+  },
+  {
+    "accountSetting.generalTab.errcode1000":
+      "Có chứa ký tự đặc biệt không đúng chỗ",
+  },
+];
+
+const json2 = [
+  { KEY: "TEST" },
+  { "accountSetting.generalTab.capEnterUserID": 123123 },
+  { "accountSetting.generalTab.contentChangeName": "" },
+  { "accountSetting.generalTab.contentUserID": 789789 },
+  { "accountSetting.generalTab.errChangeName": 9789879879 },
+  { "accountSetting.generalTab.errcode1000": 9879879789789880 },
+];
+
+const json4 = [
+  { KEY: "TEST2" },
+  { "accountSetting.generalTab.capEnterUserID": "qqqqq" },
+  { "accountSetting.generalTab.contentChangeName": "www" },
+  { "accountSetting.generalTab.contentUserID": "eee" },
+  { "accountSetting.generalTab.errChangeName": "rrrr" },
+  { "accountSetting.generalTab.errcode1000": "fffff" },
+];
+
+const json5 = [
+  { KEY: "TEST3" },
+  { "accountSetting.generalTab.capEnterUserID": "" },
+  { "accountSetting.generalTab.contentChangeName": "" },
+  { "accountSetting.generalTab.contentUserID": "" },
+  { "accountSetting.generalTab.errChangeName": "" },
+  { "accountSetting.generalTab.errcode1000": "" },
+];
